6b7e57323ea136c78c639279d690fcdc5ae62dd8
pncy1123 <mrusso1123@gmail.com>
2025-07-08 21:41:40 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,41 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('ot-cache').then(cache => {
-    return cache.addAll(['index.html']);
-  }));
+const CACHE_NAME = "overtime-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "icon.png"
+];
+
+// Install SW and cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(response => {
-    return response || fetch(e.request);
-  }));
+
+// Activate SW and clean up old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Serve cached content
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
